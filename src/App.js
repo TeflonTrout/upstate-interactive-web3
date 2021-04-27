7be2b064be02d3bfa08dfbce91453063b280799b
TeflonTrout <jtkazo@gmail.com>
2021-04-27 15:27:22 -0400

    updated functionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import Web3 from 'web3'
+import Web3 from 'web3';
 import { ABI_ADDRESS, ABI_CONTRACT } from './config.js';
 import Emoji from './Emoji.js';
 import Logo from './images/upstate-interactive.png';
@@ -12,43 +12,9 @@ function App() {
   const [count, setCount] = useState();
   const [walletBalance, setWalletBalance] = useState();
   const [isConnected, setIsConnected] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    // const init = async() => {
-    //   try {
-    //     const web3 = new Web3(Web3.givenProvider);
-    //     if (window.ethereum) {
-    //       await window.ethereum.request({ method: 'eth_requestAccounts' });
-    //       window.web3 = new Web3(window.ethereum);
-    //       setWeb3(web3);
-    //       //CHECK IF CONNECTED TO RINKEBY TEST NETWORK
-    //       web3.eth.net.getNetworkType()
-    //         .then(res => {
-    //           if(res !== 'rinkeby'){
-    //             alert("Please Connect to the Rinkeby Test Network!")
-    //           }
-    //         });
-    //       //DEFINE CONTRACT
-    //       const newContract = new web3.eth.Contract(ABI_CONTRACT, ABI_ADDRESS);
-    //       setContract(newContract);
-    //       //DEFINE METAMASK ACCOUNT
-    //       const newAccount = await web3.eth.getAccounts();
-    //       setMyAccount(newAccount[0]);
-    //       //DEFINE CURRENT COUNT
-    //       const tempCount = await newContract.methods.getCount().call();
-    //       setCount(tempCount);
-    //       setIsConnected(true);
-    //       return true;
-    //     } else {
-    //       console.log('PLEASE CONNECT METAMASK')
-    //     }
-    //   } catch (error) {
-
-    //   }
-    // }
-    // init();
-  }, [])
-
+  //Anytime 'myAccount' is changed, run the getBalance function
   useEffect(() => {
     getBalance(myAccount, web3)
   }, [myAccount])
@@ -57,9 +23,11 @@ function App() {
     try {
       const web3 = new Web3(Web3.givenProvider);
       if (window.ethereum) {
+        setIsLoading(true);
         await window.ethereum.request({ method: 'eth_requestAccounts' });
         window.web3 = new Web3(window.ethereum);
         setWeb3(web3);
+
         //CHECK IF CONNECTED TO RINKEBY TEST NETWORK
         web3.eth.net.getNetworkType()
           .then(res => {
@@ -67,43 +35,49 @@ function App() {
               alert("Please Connect to the Rinkeby Test Network!")
             }
           });
+
         //DEFINE CONTRACT
         const newContract = new web3.eth.Contract(ABI_CONTRACT, ABI_ADDRESS);
         setContract(newContract);
+
         //DEFINE METAMASK ACCOUNT
         const newAccount = await web3.eth.getAccounts();
         setMyAccount(newAccount[0]);
+
         //DEFINE CURRENT COUNT
-        const tempCount = await newContract.methods.getCount().call();
+        const tempCount = await newContract.methods.getCount().call({ from: newAccount[0] });
         setCount(tempCount);
-        //DEFINE ISCONNECTED
         setIsConnected(true);
+        setIsLoading(false);
         return true;
       } else {
         setIsConnected(false);
         console.log('PLEASE CONNECT TO METAMASK')
       }
     } catch (error) {
-      console.error(error)
+      if(web3 !== undefined) {
+        console.error(error)
+      }
     }
   }
 
   const getBalance = async (myAccount, web3) => {
     try {
-      const balance = await web3.eth.getBalance(myAccount);
-      const toDecimal = await web3.utils.fromWei(balance);
-      setWalletBalance(toDecimal);
+      if(web3 !== undefined){
+        const balance = await web3.eth.getBalance(myAccount);
+        //Convert balance from Wei to Ether
+        const toDecimal = await web3.utils.fromWei(balance);
+        setWalletBalance(toDecimal.slice(0,6));
+      }
     } catch (error) {
+      console.error(error)
     }
   };
 
-  const handleAccountChange = async () => {
-    await loadBlockchainData()
-      .then(getBalance(myAccount, web3))
-  }
-
   const web3Connect = async () => {
-      handleAccountChange();
+    if (window.ethereum.isConnected() === true) {
+      await loadBlockchainData();
+    }
   }
 
   const getCount = async () => {
@@ -111,7 +85,7 @@ function App() {
       const getCount = await contract.methods.getCount().call({ from: myAccount });
       setCount(getCount)
     } else {
-      console.log('PLEASE CONNECT TO METAMASK')
+      console.error('PLEASE CONNECT TO METAMASK')
     }
   }
 
@@ -121,7 +95,7 @@ function App() {
       const increase = await contract.methods.increment().send({ from:myAccount });
       return increase;
     } else {
-      console.log('PLEASE CONNECT TO METAMASK')
+      console.error('PLEASE CONNECT TO METAMASK')
     }
   }
 
@@ -131,14 +105,15 @@ function App() {
       const decrease = await contract.methods.decrement().send({ from:myAccount });
       return decrease;
     } else {
-      console.log('PLEASE CONNECT TO METAMASK')
+      console.error('PLEASE CONNECT TO METAMASK')
     }
   }
 
   //Check to see if a Web3 provider is installed
   if(window.ethereum) {
+    
     //Handle an account change
-    window.ethereum.on('accountsChanged', handleAccountChange);
+    window.ethereum.on('accountsChanged', web3Connect);
     window.ethereum.isConnected();
   } else {
     console.error("*** PLEASE USE A METAMASK COMPATIBLE BROWSER ***")
@@ -153,30 +128,36 @@ function App() {
           <button 
             className='metamask-btn' 
             style={{display: isConnected ? 'none' : 'block'}}
-            onClick={e => web3Connect(e)}>
+            onClick={e => web3Connect(e)}
+          >
             Connect MetaMask 
             <Emoji symbol="🦊" label="fox"/>
           </button>
         <div className="account-container">
-          <h1 style={{
-            color: isConnected ? 'black' : 'darkred',
-            fontSize: isConnected ? '22px' : '16px'
+          <h1 style={{display: isLoading ? 'flex' : 'none'}}>Loading...</h1>
+          <h1
+            style={{
+              display: isLoading ? 'none' : '',
+              color: isConnected ? 'black' : 'darkred',
+              fontSize: isConnected ? '28px' : '16px'
             }}>
-            {isConnected ? `Account: ${myAccount}` : "Please Connect MetaMask"}
+            {isConnected ? `Account: ${myAccount.slice(0,10)}...` : "Please Connect MetaMask"}
           </h1>
         </div>
         <div className="balance-container">
           <h2>
-            Balance: {walletBalance ? walletBalance : "0"} ETH
+            {isLoading ? 
+            ''
+            : `Balance: ${walletBalance ? walletBalance : "0"} ETH`
+          }
           </h2>
         </div>
         <div className="info-container">
-          
           <div className='button-container'>
             <button className='increase-btn' onClick={e => handleIncrease(e)}>Increment</button>
             <button className='decrease-btn' onClick={e => handleDecrease(e)}>Decrement</button>
           </div>
-          <h3>Count: {count}</h3>
+          <h2>Current Count: {count}</h2>
           <button className='get-count-btn' onClick={e => getCount(e)}>Get Count</button>
         </div>
       </div>
