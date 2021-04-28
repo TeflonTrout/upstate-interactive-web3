1b1cad629e08aab5d05d3c7b36ad02f82393d385
TeflonTrout <jtkazo@gmail.com>
2021-04-28 12:56:20 -0400

    added snackbars and other network functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { BrowserRouter as Router, NavLink } from 'react-router-dom';
 import Web3 from 'web3';
 import { ABI_ADDRESS, ABI_CONTRACT } from './config.js';
+import { Snackbar, IconButton } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
 import Emoji from './Emoji.js';
 import Logo from './images/upstate-interactive.png';
 import './Style.css';
@@ -13,10 +16,13 @@ function App() {
   const [walletBalance, setWalletBalance] = useState();
   const [isConnected, setIsConnected] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [network, setNetwork] = useState();
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [failedSnackbarOpen, setFailedSnackbarOpen] = useState(false);
 
   //Anytime 'myAccount' is changed, run the getBalance function
   useEffect(() => {
-    getBalance(myAccount, web3)
+    getBalance(myAccount, web3);
   }, [myAccount])
 
   async function loadBlockchainData() {
@@ -31,9 +37,12 @@ function App() {
         //CHECK IF CONNECTED TO RINKEBY TEST NETWORK
         web3.eth.net.getNetworkType()
           .then(res => {
+            setNetwork(res);
             if(res !== 'rinkeby'){
-              alert("Please Connect to the Rinkeby Test Network!")
-            }
+              alert('Please Connect to the Rinkeby Test Network!')
+              setIsLoading(false);
+            } 
+            return;
           });
 
         //DEFINE CONTRACT
@@ -62,28 +71,30 @@ function App() {
   }
 
   const getBalance = async (myAccount, web3) => {
-    try {
-      if(web3 !== undefined){
+    if(web3 !== undefined){
         const balance = await web3.eth.getBalance(myAccount);
         //Convert balance from Wei to Ether
         const toDecimal = await web3.utils.fromWei(balance);
         setWalletBalance(toDecimal.slice(0,6));
-      }
-    } catch (error) {
-      console.error(error)
     }
   };
 
   const web3Connect = async () => {
-    if (window.ethereum.isConnected() === true) {
-      await loadBlockchainData();
+    try {
+      if(window.ethereum.isConnected() === true) {
+        await loadBlockchainData()
+          .then(getBalance(myAccount, web3))
+      }
+    } catch (error) {
+      console.error(error)
     }
   }
 
   const getCount = async () => {
     if(contract !== undefined) {
       const getCount = await contract.methods.getCount().call({ from: myAccount });
-      setCount(getCount)
+      setCount(getCount);
+      setSnackbarOpen(true);
     } else {
       console.error('PLEASE CONNECT TO METAMASK')
     }
@@ -92,7 +103,9 @@ function App() {
   const handleIncrease = async (e) => {
     e.preventDefault();
     if(contract !== undefined) {
-      const increase = await contract.methods.increment().send({ from:myAccount });
+      const increase = await contract.methods.increment().send({ from:myAccount })
+        .on('receipt', function(){setSnackbarOpen(true)})
+        .on('error', function(){setFailedSnackbarOpen(true)})
       return increase;
     } else {
       console.error('PLEASE CONNECT TO METAMASK')
@@ -102,8 +115,10 @@ function App() {
   const handleDecrease = async (e) => {
     e.preventDefault();
     if(contract !== undefined) {
-      const decrease = await contract.methods.decrement().send({ from:myAccount });
-      return decrease;
+      const decrease = await contract.methods.decrement().send({ from:myAccount })
+        .on('receipt', function(){setSnackbarOpen(true)})
+        .on('error', function(){setFailedSnackbarOpen(true)})
+      return decrease
     } else {
       console.error('PLEASE CONNECT TO METAMASK')
     }
@@ -111,48 +126,74 @@ function App() {
 
   //Check to see if a Web3 provider is installed
   if(window.ethereum) {
-    
     //Handle an account change
     window.ethereum.on('accountsChanged', web3Connect);
     window.ethereum.isConnected();
   } else {
-    console.error("*** PLEASE USE A METAMASK COMPATIBLE BROWSER ***")
+    console.error('*** PLEASE USE A METAMASK COMPATIBLE BROWSER ***')
   }
 
+  function capitalizeFirstLetter(network) {
+    if (network !== undefined) {
+      return network.charAt(0).toUpperCase() + network.slice(1);
+    }
+  }
+
+  const closeSnackbar = (e, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false)
+  }
+
+  const closeFailedSnackbar = (e, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setFailedSnackbarOpen(false)
+  }
+ 
   return (
-    <div className="App">
-      <div className="logo">
-        <img src={Logo} alt="Upstate Interactive"/>
+    <div className='App'>
+      <div className='logo'>
+        <img src={Logo} alt='Upstate Interactive'/>
       </div>
-      <div className="hero">
-          <button 
-            className='metamask-btn' 
-            style={{display: isConnected ? 'none' : 'block'}}
-            onClick={e => web3Connect(e)}
-          >
-            Connect MetaMask 
-            <Emoji symbol="🦊" label="fox"/>
-          </button>
-        <div className="account-container">
-          <h1 style={{display: isLoading ? 'flex' : 'none'}}>Loading...</h1>
+      <div className='extra-info-container'>
+        {isConnected ? <h3>Connected to: {capitalizeFirstLetter(network)} Network</h3> : '' }
+      </div>
+      <div className='hero'>
           <h1
             style={{
               display: isLoading ? 'none' : '',
               color: isConnected ? 'black' : 'darkred',
               fontSize: isConnected ? '28px' : '16px'
             }}>
-            {isConnected ? `Account: ${myAccount.slice(0,10)}...` : "Please Connect MetaMask"}
+            {isConnected ? `Account: ${myAccount.slice(0,10)}...` : 'Please Connect MetaMask'}
           </h1>
+          <button 
+            className='metamask-btn' 
+            style={{display: isConnected ? 'none' : 'block'}}
+            onClick={e => web3Connect(e)}
+          >
+            Connect MetaMask 
+            <Emoji symbol='🦊' label='fox'/>
+          </button>
+          {isConnected
+          ? ""
+          : <h5 onClick={function(){window.open('https://metamask.io', "_blank")}}>Don't have a MetaMask Account?</h5>
+          }
+        <div className='account-container'>
+          <h1 style={{display: isLoading ? 'flex' : 'none'}}>Loading...</h1>
         </div>
-        <div className="balance-container">
+        <div className='balance-container'>
           <h2>
-            {isLoading ? 
-            ''
-            : `Balance: ${walletBalance ? walletBalance : "0"} ETH`
+            {isLoading
+            ? ''
+            : `Balance: ${walletBalance ? walletBalance : '0'} ETH`
           }
           </h2>
         </div>
-        <div className="info-container">
+        <div className='info-container'>
           <div className='button-container'>
             <button className='increase-btn' onClick={e => handleIncrease(e)}>Increment</button>
             <button className='decrease-btn' onClick={e => handleDecrease(e)}>Decrement</button>
@@ -161,6 +202,40 @@ function App() {
           <button className='get-count-btn' onClick={e => getCount(e)}>Get Count</button>
         </div>
       </div>
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={snackbarOpen}
+        autoHideDuration={5000}
+        onClose={e => closeSnackbar(e)}
+        message="Transaction Confirmed"
+        action={
+          <div>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={e => closeSnackbar(e)}>
+              <CloseIcon fontSize="small" />
+            </IconButton>
+          </div>
+        }
+      />
+      <Snackbar
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        open={failedSnackbarOpen}
+        autoHideDuration={5000}
+        onClose={e => closeFailedSnackbar(e)}
+        message="Transaction Failed"
+        action={
+          <div>
+            <IconButton size="small" aria-label="close" color="inherit" onClick={e => closeFailedSnackbar(e)}>
+              <CloseIcon size='small' />
+            </IconButton>
+          </div>
+        }
+      />
     </div>
   );
 }
